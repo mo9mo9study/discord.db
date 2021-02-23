5ab5b623cc8339bf649459fa0b4a24bdb8625aaf
su-pleiades <su.impreza.itengineer@gmail.com>
2021-02-23 15:23:42 +0900

    管理外.envを元にsampleファイルを作成

--- mo9mo9db/dbsession.py
+++ mo9mo9db/dbsession.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy_utils import database_exists, create_database, drop_database
+from sqlalchemy.ext.declarative import declarative_base
 
 import dbconfig as cfg
 
+
+Base = declarative_base()
 
 def get_db_session():
     """
@@ -21,4 +25,8 @@ def get_db_session():
 def get_db_engine():
     db_path = f'mysql+pymysql://{cfg.DB_user}:{cfg.DB_password}@{cfg.DB_host}:{cfg.DB_port}/{cfg.DB_database}'
     engine = create_engine(db_path, encoding='utf-8', pool_size=5, convert_unicode=True, echo=True)
+        if not database_exists(engine.url): # DBの存在チェックと作成用
+            create_database(engine.url)
+    Base.metadata.create_all(engine)
+    #Base.metadata.drop_all(engine) #開発用
     return engine
