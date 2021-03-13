a3eb6203492434013d5e77ef941cdd46a5429f70
su-pleiades <su.impreza.itengineer@gmail.com>
2021-03-13 14:59:36 +0900

    PEP8に準拠した形に修正
    - 97文字制限のエラーをなくすため、関数の引数を複数行に分割
    - 使用していないモジュール列を削除

--- mo9mo9db/dbtables.py
+++ mo9mo9db/dbtables.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from sqlalchemy import Column, String, Integer, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,6 +7,7 @@ from dbsession import get_db_engine
 engine = get_db_engine()
 Base = declarative_base()
 
+
 class Studytimelogs(DBBaseMixin, Base):
     study_dt = Column(DateTime, unique=False)
     guild_id = Column(String(20), unique=False)
@@ -17,7 +16,13 @@ class Studytimelogs(DBBaseMixin, Base):
     studytime_min = Column(Integer, unique=False)
     tag_id = Column(String(20), unique=False)
 
-    def __init__(self, study_dt=None, guild_id=None, member_id=None, voice_id=None, studytime_min=None, studytag_no=None):
+    def __init__(self,
+                 study_dt=None,
+                 guild_id=None,
+                 member_id=None,
+                 voice_id=None,
+                 studytime_min=None,
+                 studytag_no=None):
         self.study_dt = study_dt
         self.guild_id = guild_id
         self.member_id = member_id
@@ -34,29 +39,40 @@ class Studymembers(DBBaseMixin, Base):
     times_id = Column(String(20), unique=True)
     joined_dt = Column(DateTime, unique=False)
     organize = Column(Boolean)
-    enrollment = Column(Boolean) 
-    
-    def __init__(self, guild_id=None, member_id=None, member_name=None, selfintroduction_id=None, times_id=None, joined_dt=None, organize=None, enrollment=None):
+    enrollment = Column(Boolean)
+
+    def __init__(self,
+                 guild_id=None,
+                 member_id=None,
+                 member_name=None,
+                 selfintroduction_id=None,
+                 times_id=None,
+                 joined_dt=None,
+                 organize=None,
+                 enrollment=None):
         self.guild_id = guild_id
         self.member_id = member_id
         self.member_name = member_name
         self.selfintroduction_id = selfintroduction_id
         self.times_id = times_id
-        self.joined_dt = joined_dt 
-        self.organize = organize 
+        self.joined_dt = joined_dt
+        self.organize = organize
         self.enrollment = enrollment
-    
+
 
 class Studytags(DBBaseMixin, Base):
     member_id = Column(String(20), unique=True)
     tag_name = Column(String(40), unique=True)
     tag_default = Column(Boolean)
 
-    def __init__(self, member_id=None, tag_name=None, tag_default=None):
+    def __init__(self,
+                 member_id=None,
+                 tag_name=None,
+                 tag_default=None):
         self.member_id = member_id
         self.tag_name = tag_name
         self.tag_default = tag_default
 
 
-#Base.metadata.drop_all(bind=engine)
+# Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
