9f450d369f3713a2973a49ac2cd500f525339f1d
su-pleiades <su.impreza.itengineer@gmail.com>
2021-03-13 15:31:40 +0900

    CRUDテスト用のコードのパスを変更

--- test-crud.py
+++ test-crud.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 import sys
-import inspect
 from pprint import pprint
 
-from dbtables import Studytimelogs, Studymembers, Studytags
+dbmodule_path = "/home/centos/work/discord.db/mo9mo9db"
+sys.path.append(dbmodule_path)
+from dbtables import Studytimelogs, Studymembers, Studytags  # noqa: E402, F401
+
 
 arg = ""
 args = sys.argv
 
 print(f"引数の数：{len(args)}({args})")
-if len(args) > 1: 
+if len(args) > 1:
     arg = args[1]
     session = Studymembers.session()
 else:
@@ -17,36 +19,30 @@ else:
     sys.exit()
 
 if arg == "insert":
-    # insert
+    # Insert
     obj = Studymembers(guild_id="470074961617354773",
-                            member_id="603567991132782592",
-                            member_name="SuPleiades",
-                            joined_dt="2021-01-15 09:05:01")
+                       member_id="603567991132782592",
+                       member_name="SuPleiades",
+                       joined_dt="2021-01-15 09:05:01")
     Studymembers.insert(obj)
 elif arg == "update":
     # Update
     obj = Studymembers.objects(session).first()
     obj.enrollment = True
-    obj.organize = False  
+    obj.organize = False
     obj.save(session)
-    #pprint(inspect.getmembers(obj), indent=4)
+    # pprint(inspect.getmembers(obj), indent=4)
 elif arg == "delete":
+    # Delete
     obj = Studymembers.objects(session).first()
     obj.delete(session)
 elif arg == "select":
+    # Select
     member_id = "603567991132782592"
-    obj = Studymembers.objects(session).filter(Studymembers.member_id == member_id).first()
-    #pprint(inspect.getmembers(obj.__dict__), indent=4)
+    obj = Studymembers.objects(session).filter(
+        Studymembers.member_id == member_id).first()
+    # pprint(inspect.getmembers(obj.__dict__), indent=4)
     pprint(obj.__dict__, indent=4)
 else:
     print("引数に[insert/update/delete/select]を指定してください")
     sys.exit()
-
-
-# select
-
-# Delete
-    
-
-
-
